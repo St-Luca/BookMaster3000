deeb3645d0262159512d657a665c202a39baeb87
St-Luca <lukyancenkosasha277@gmail.com>
2024-10-27 23:34:35 +0500

    search pagination + RenewReturnDateByWeek method

--- Application/MapsterConfig.cs
+++ Application/MapsterConfig.cs
@@ -9,6 +9,12 @@ public static class MapsterConfig
     public static void RegisterMappings()
     {
         TypeAdapterConfig<ClientCardDto, ClientCard>.NewConfig()
+            .TwoWays()
             .IgnoreNullValues(true);
+
+        TypeAdapterConfig<Book, BookDto>
+            .NewConfig()
+            .Map(dest => dest.BookAuthors, src => src.BookAuthors.Select(ba => ba.Author.Name).ToList())
+            .Map(dest => dest.BookSubjects, src => src.BookSubjects.Select(bs => bs.Subject.Name).ToList());
     }
 }
