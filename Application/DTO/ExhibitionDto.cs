c4ba2fcfea5edc2debdda7a60958e9dae0339c30
St-Luca <lukyancenkosasha277@gmail.com>
2024-12-19 02:12:59 +0500

    fix date

--- Application/DTO/ExhibitionDto.cs
+++ Application/DTO/ExhibitionDto.cs
@@ -3,7 +3,7 @@
 public class ExhibitionDto
 {
     public int Id { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public ICollection<BookDto> Books { get; set; } = [];
--- Domain/Entities/Exhibition.cs
+++ Domain/Entities/Exhibition.cs
@@ -3,7 +3,7 @@
 public class Exhibition
 {
     public int Id { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public ICollection<ExhibitionBook> ExhibitionBooks { get; set; } = [];
