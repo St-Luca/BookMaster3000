3418976452a99f084e335d132e7347939755d738
St-Luca <lukyancenkosasha277@gmail.com>
2024-12-19 11:24:39 +0500

    added client data to circulation record

--- Application/DTO/CirculationRecord.cs
+++ Application/DTO/CirculationRecord.cs
@@ -5,7 +5,8 @@ public class CirculationRecord
     public int BookId { get; set; }
     public string BookTitle { get; set; }
     public string BookSubtitle { get; set; }
-    
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
     public DateTime IssueFrom { get; set; }
     public DateTime IssueTo { get; set; }
     public DateTime? ReturnDate { get; set; }
